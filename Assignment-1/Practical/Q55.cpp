dc4e95e04af3c130304fac5dc3902862e0742cb6
Harshwardhan-1 <harshwardhany87@gmail.com>
2025-04-29 12:26:13 +0530

    Create Q55.cpp

---
+++ Assignment-1/Practical/Q55.cpp
@@ -0,0 +1,33 @@
+//Q55.write a c++ program to find the transpose of a given matrix using nesterd for loops.
+#include <iostream>
+using namespace std;
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+    int matrix[100][100], transpose[100][100];
+    cout << "Enter the elements of the matrix:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << "Element [" << i << "][" << j << "]: ";
+            cin >> matrix[i][j];
+        }
+    }
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    cout << "\nTranspose of the matrix:\n";
+    for (int i = 0; i < cols; ++i) {
+        for (int j = 0; j < rows; ++j) {
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
