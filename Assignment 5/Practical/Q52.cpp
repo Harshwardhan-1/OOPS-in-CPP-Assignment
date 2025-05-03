ec103adbf06cd9ecfb1a3142fcd4c9325d3313d1
Harshwardhan-1 <harshwardhany87@gmail.com>
2025-05-03 16:21:43 +0530

    Create Q52.cpp

---
+++ Assignment 5/Practical/Q52.cpp
@@ -0,0 +1,11 @@
+//Q52.Implement a program that uses streams to read integers from the user and display their sum.
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2,sum;
+    cout<<"entre number:"<<endl;
+    cin>>num1>>num2;
+    sum=num1+num2;
+    cout<<"the sum is:"<<sum<<endl;
+    return 0;
+}
