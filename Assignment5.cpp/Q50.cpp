dbebc2b7b65b6f3ea21518c1e319e031d36ebca6
Harshwardhan-1 <harshwardhany87@gmail.com>
2025-05-03 16:08:40 +0530

    Add files via upload

---
+++ Assignment5.cpp/Q50.cpp
@@ -0,0 +1,10 @@
+//Q50.Write a program to perform basic input and output using streams (cin and cout).
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"enter number:"<<endl;
+    cin>>number;
+    cout<<"your entered number is:"<<number<<endl;
+    return 0;
+}
